66ca568271ba4d5711baa473c8e2b3443fea4d0d
Khurram Ali <65960587+destocot@users.noreply.github.com>
2024-03-03 23:11:50 -0500

    added headed to landing page (#4)

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,45 @@
 import { Routes } from "@config/routes";
 import styles from "./index.module.scss";
 
+const links = [
+  { href: "/", label: "Home" },
+  { href: "/products", label: "Products" },
+  { href: "/documentation", label: "Documentation" },
+  { href: "/pricing", label: "Pricing" },
+];
+
 const IssuesPage = () => {
   return (
     <div>
       <header className={styles.header}>
-        {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src="/icons/logo-large.svg" alt="Prolog logo" />
-        <a href={Routes.projects}>Dashboard</a>
+        <div className={styles.container}>
+          <div className={styles.content}>
+            <div className={styles.logoContainer}>
+              {/* eslint-disable-next-line @next/next/no-img-element */}
+              <img
+                src="/icons/logo-large.svg"
+                alt="Prolog logo"
+                className={styles.logo}
+              />
+            </div>
+            <nav>
+              <ul className={styles.navContainer}>
+                {links.map(({ href, label }) => (
+                  <li key={href}>
+                    <a href={href} className={styles.navLink}>
+                      {label}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+            <div className={styles.dashboardLinkContainer}>
+              <a href={Routes.projects} className={styles.dashboardLink}>
+                Open Dashboard
+              </a>
+            </div>
+          </div>
+        </div>
       </header>
       <button
         className={styles.contactButton}
