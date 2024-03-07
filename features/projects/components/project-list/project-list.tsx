830dead44a15cd42f4af215c68367aeccbd98a58
destocot <khurramcali@gmail.com>
2024-03-07 15:44:44 -0500

    created error message for failed projects api request with cypress test

--- features/projects/components/project-list/project-list.tsx
+++ features/projects/components/project-list/project-list.tsx
@@ -3,15 +3,30 @@ import { useGetProjects } from "../../api/use-get-projects";
 import styles from "./project-list.module.scss";
 
 export function ProjectList() {
-  const { data, isLoading, isError, error } = useGetProjects();
+  const { data, isLoading, isError, error, isRefetching, refetch } =
+    useGetProjects();
 
-  if (isLoading) {
+  if (isLoading || isRefetching) {
     return <div>Loading</div>;
   }
 
   if (isError) {
     console.error(error);
-    return <div>Error: {error.message}</div>;
+    return (
+      <div className={styles.error}>
+        <div className={styles.errorContent}>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src="/icons/error-alert-circle.svg" alt="error alert icon" />
+          <span className={styles.errorMessage}>
+            There was a problem while loading the project data
+          </span>
+          <button className={styles.errorButton} onClick={() => refetch()}>
+            Try again {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img src="/icons/error-arrow-right.svg" alt="error arrow icon" />
+          </button>
+        </div>
+      </div>
+    );
   }
 
   return (
